61ea4a9f493c35342eb17c0d2abbe51e4fdd9bc6
paszczi <maciej.paszta@gmail.com>
2011-11-28 20:00:41 +0100

    Determine whether provided header is a vorbis stream header

---
+++ Mlabs.Ogg/src/Streams/Vorbis/VorbisHeaderInfo.cs
@@ -0,0 +1,17 @@
+﻿namespace Mlabs.Ogg.Streams.Vorbis
+{
+    /// <summary>
+    /// Information needed to read vorbis header
+    /// </summary>
+    public class VorbisHeaderInfo
+    {
+        public const int HeaderTypeIndex = 0;
+        public const int IdentificatationHeaderSize = 30;
+        public const int IdentificartionHeader = 1;
+        public const int MagicSeqIndex = 1;
+        public const int MagicSeqSize = 6;
+        public const string MagicSeq = "vorbis";
+        public const int VersionIndex = 7;
+        public const uint VorbisVersion = 0;
+    }
+}
--- Mlabs.Ogg/src/Streams/Vorbis/VorbisStreamDecoder.cs
+++ Mlabs.Ogg/src/Streams/Vorbis/VorbisStreamDecoder.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using Mlabs.Ogg.Metadata;
 
 namespace Mlabs.Ogg.Streams.Vorbis
@@ -19,10 +20,43 @@ namespace Mlabs.Ogg.Streams.Vorbis
                 return false;
 
             byte[] header = Read(firstPage.Segments.First().FileOffset, GetHeaderSize(firstPage));
-            return false;
+            if (!IsProperSize(header))
+                return false;
+            if (!IsProperHeaderType(header))
+                return false;
+            if (!HasMagicSeq(header))
+                return false;
+            if (!HasProperVersion(header))
+                return false;
+            return true;
         }
 
-        private int GetHeaderSize (Page p)
+        private bool HasProperVersion(byte[] header)
+        {
+            uint version = BitConverter.ToUInt32(header, VorbisHeaderInfo.VersionIndex);
+            return version == VorbisHeaderInfo.VorbisVersion;
+        }
+
+        private bool IsProperSize(byte[] header)
+        {
+            return header.Length == VorbisHeaderInfo.IdentificatationHeaderSize;
+        }
+
+        private bool IsProperHeaderType(byte[] header)
+        {
+            //header must be the identification header
+            return header[VorbisHeaderInfo.HeaderTypeIndex] == VorbisHeaderInfo.IdentificartionHeader;
+        }
+
+        private bool HasMagicSeq(byte[] header)
+        {
+            string magicSeq = Encoding.ASCII.GetString(header, VorbisHeaderInfo.MagicSeqIndex, VorbisHeaderInfo.MagicSeqSize);
+            if (magicSeq != VorbisHeaderInfo.MagicSeq)
+                return false;
+            return true;
+        }
+
+        private int GetHeaderSize(Page p)
         {
             int size = 0;
             foreach (var segment in p.Segments)
