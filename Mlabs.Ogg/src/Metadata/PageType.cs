91bc516a01ff3e8f749d6a7d5ab41d2b4814850f
paszczi <maciej.paszta@gmail.com>
2011-11-28 19:36:11 +0100

    HeaderType -> PageType

---
+++ Mlabs.Ogg/src/Metadata/PageType.cs
@@ -0,0 +1,28 @@
+﻿namespace Mlabs.Ogg.Metadata
+{
+    public enum PageType : byte
+    {
+        /// <summary>
+        /// Page is normal page
+        /// </summary>
+        Normal = 0x00,
+
+
+        /// <summary>
+        /// Page is a continuation of a previous page
+        /// </summary>
+        Continuation = 0x01,
+
+
+        /// <summary>
+        /// Page is the beginning of a stream
+        /// </summary>
+        BeginningOfStream = 0x02,
+
+
+        /// <summary>
+        /// Page is the end of the stream
+        /// </summary>
+        EndOfStream = 0x04,
+    }
+}
