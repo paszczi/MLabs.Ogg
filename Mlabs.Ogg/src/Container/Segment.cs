1c6a9caa5e96df679d69ecd59b5226ffaa46ce9d
paszczi <maciej.paszta@gmail.com>
2011-11-29 18:12:24 +0100

    ToString() for Segment

--- Mlabs.Ogg/src/Container/Segment.cs
+++ Mlabs.Ogg/src/Container/Segment.cs
@@ -4,5 +4,10 @@
     {
         public int Size { get; set; }
         public long FileOffset { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Size: {0}, FileOffset: {1}", Size, FileOffset);
+        }
     }
 }
